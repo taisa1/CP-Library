20e3b6c3a95ee6f075c8926e059620da4767c951
TAISA1 <osa.kento.81@gmail.com>
2018-06-22 20:07:45 +0900

    [add]matching

---
+++ Graph/Max-Matching.cpp
@@ -0,0 +1,39 @@
+struct Maxmatch{
+    int V;
+    vector<int> G[100010];
+    int match[100010];
+    bool used[100010];
+    Maxmatch(int n){V=n;}
+    
+    void add_edge(int u,int v){
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    
+    bool dfs(int v){
+        used[v]=true;
+        for(int i=0;i<G[v].size();i++){
+            int u=G[v][i],w=match[u];
+            if(w<0||!used[w]&&dfs(w)){
+                match[v]=u;
+                match[u]=v;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int matching(){
+        int res=0;
+        memset(match,-1,sizeof(match));
+        for(int v=0;v<V;v++){
+            if(match[v]<0){
+                memset(used,0,sizeof(used));
+                if(dfs(v)){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
