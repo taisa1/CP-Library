e06e148034338a4c0d743fb38c4069eb577e628c
TAISA_ <osa.kento.81@gmail.com>
2020-04-11 22:25:31 +0900

    update roliha

--- String/RollingHash.cpp
+++ String/RollingHash.cpp
@@ -1,47 +1,36 @@
-//from https://github.com/tatyam-prime/kyopro_library/blob/master/RollingHash.cpp
-using ull = unsigned long long;
+using ull = uint64_t;
 const int bases[64] = {257, 262, 266, 275, 276, 281, 285, 290, 296, 302, 306,
                        310, 311, 313, 323, 333, 344, 345, 350, 357, 367, 370,
                        373, 402, 423, 425, 431, 440, 442, 443, 454, 457, 458,
                        462, 471, 478, 481, 487, 489, 492, 499, 501, 502, 503,
                        506, 514, 524, 532, 535, 541, 550, 552, 557, 559, 562,
                        563, 567, 570, 571, 580, 592, 597, 604, 612};
-const ull mod = 0x1fffffffffffffff,
-          base = bases[chrono::duration_cast<chrono::microseconds>(
-                           chrono::system_clock::now().time_since_epoch())
-                           .count() &
-                       63];
+const ull rmod = 0x1fffffffffffffff;
+const ull base = bases[chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count() & 63];
+const ull mask30 = (1UL << 30) - 1, mask31 = (1UL << 31) - 1;
 struct RollingHash {
-    vector<ull> hashed, power;
-    static constexpr ull mask(int a) { return (1ull << a) - 1; }
-    inline ull mul(ull a, ull b) const {
-        ull a31 = a >> 31, b31 = b >> 31;
-        a &= mask(31);
-        b &= mask(31);
-        ull x = a * b31 + b * a31;
-        ull ans = (a31 * b31 << 1) + (x >> 30) + ((x & mask(30)) << 31) + a * b;
-        ans = (ans >> 61) + ans & mask(61);
-        if (ans >= mod)
-            ans -= mod;
-        return ans;
+    vector<ull> hash, pow;
+    inline ull mul(const ull &a, const ull &b) const {
+        ull mid = (a & mask31) * (b >> 31) + (a >> 31) * (b & mask31);
+        ull res = (a >> 31) * (b >> 31) * 2 + (mid >> 30) + ((mid & mask30) << 31);
+        res += (a & mask31) * (b & mask31);
+        res = (res >> 61) + (res & rmod);
+        if (res >= rmod) res -= rmod;
+        return res;
     }
     RollingHash(const string &s) {
-        ll n = s.size();
-        hashed.assign(n + 1, 0);
-        power.assign(n + 1, 0);
-        power[0] = 1;
-        for (ll i = 0; i < n; i++) {
-            power[i + 1] = mul(power[i], base);
-            hashed[i + 1] = mul(hashed[i], base) + s[i];
-            if (hashed[i + 1] >= mod)
-                hashed[i + 1] -= mod;
+        hash.resize(s.length() + 1);
+        pow.resize(s.length() + 1);
+        pow[0] = 1;
+        for (int i = 0; i < s.length(); i++) {
+            hash[i + 1] = mul(hash[i], base) + s[i];
+            pow[i + 1] = mul(pow[i], base);
+            if (hash[i + 1] >= rmod) hash[i + 1] -= rmod;
         }
     }
-
-    ull get(ll l, ll r) const {
-        ull ret = hashed[r] + mod - mul(hashed[l], power[r - l]);
-        if (ret >= mod)
-            ret -= mod;
-        return ret;
+    ull get(int l, int r) { //[l,r]
+        ull res = hash[r + 1] + rmod - mul(hash[l], pow[r + 1 - l]);
+        if (res >= rmod) res -= rmod;
+        return res;
     }
 };
