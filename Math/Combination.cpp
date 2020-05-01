4a8b5e474128f4697da681d312969bf529381a40
TAISA_ <osa.kento.81@gmail.com>
2020-05-01 22:38:01 +0900

    change combination

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -1,4 +1,80 @@
-ector<mint> f, fi;
+//from http://noshi91.hatenablog.com/entry/2019/03/31/174006
+template <std::uint_fast64_t Modulus>
+class modint {
+    using u64 = std::uint_fast64_t;
+
+  public:
+    u64 a;
+    constexpr modint(const u64 x = 0) noexcept : a(x % Modulus) {}
+    constexpr u64 &value() noexcept { return a; }
+    constexpr const u64 &value() const noexcept { return a; }
+    constexpr modint operator+(const modint rhs) const noexcept {
+        return modint(*this) += rhs;
+    }
+    constexpr modint operator-(const modint rhs) const noexcept {
+        return modint(*this) -= rhs;
+    }
+    constexpr modint operator*(const modint rhs) const noexcept {
+        return modint(*this) *= rhs;
+    }
+    constexpr modint operator/(const modint rhs) const noexcept {
+        return modint(*this) /= rhs;
+    }
+    constexpr modint operator^(const u64 rhs) const noexcept {
+        return modint(*this) ^= rhs;
+    }
+    constexpr modint &operator+=(const modint rhs) noexcept {
+        a += rhs.a;
+        if (a >= Modulus) {
+            a -= Modulus;
+        }
+        return *this;
+    }
+    constexpr modint &operator-=(const modint rhs) noexcept {
+        if (a < rhs.a) {
+            a += Modulus;
+        }
+        a -= rhs.a;
+        return *this;
+    }
+    constexpr modint &operator*=(const modint rhs) noexcept {
+        a = a * rhs.a % Modulus;
+        return *this;
+    }
+    constexpr modint &operator/=(modint rhs) noexcept {
+        u64 exp = Modulus - 2;
+        while (exp) {
+            if (exp % 2) {
+                *this *= rhs;
+            }
+            rhs *= rhs;
+            exp /= 2;
+        }
+        return *this;
+    }
+    constexpr modint &operator^=(u64 exp) {
+        modint rhs = modint(*this);
+        a = 1;
+        while (exp) {
+            if (exp % 2) {
+                *this *= rhs;
+            }
+            rhs *= rhs;
+            exp /= 2;
+        }
+        return *this;
+    }
+    friend ostream &operator<<(ostream &os, const modint &x) {
+        os << x.a;
+        return os;
+    }
+    friend istream &operator>>(istream &is, modint &x) {
+        is >> x.a;
+        return is;
+    }
+};
+using mint = modint<MOD>;
+vector<mint> f, fi;
 void comb(int n) {
     f.resize(n + 1);
     fi.resize(n + 1);
