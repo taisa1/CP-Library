aaa71686870f71ea3a7d0628964df9662f6cf512
TAISA_ <osa.kento.81@gmail.com>
2021-09-12 12:37:37 +0900

    upd median

--- DataStructure/Median.cpp
+++ DataStructure/Median.cpp
@@ -1,31 +1,52 @@
 //@docs Docs/Median.md
 template <class T>
-struct Median {
-    priority_queue<T> smq;
-    priority_queue<T, vector<T>, greater<T>> lgq;
+struct Med {
+    multiset<T> lg;
+    multiset<T, greater<T>> sm;
     void push(T x) {
-        if (smq.empty()) {
-            smq.push(x);
+        if (sm.empty()) {
+            sm.insert(x);
             return;
         }
-        T t = smq.top();
-        if (smq.size() > lgq.size()) {
+        T t = *sm.begin();
+        if (sm.size() > lg.size()) {
             if (t <= x) {
-                lgq.push(x);
+                lg.insert(x);
             } else {
-                lgq.push(t);
-                smq.pop();
-                smq.push(x);
+                lg.insert(t);
+                sm.erase(sm.find(t));
+                sm.insert(x);
             }
         } else {
             if (t >= x) {
-                smq.push(x);
+                sm.insert(x);
             } else {
-                lgq.push(x);
-                smq.push(lgq.top());
-                lgq.pop();
+                lg.insert(x);
+                sm.insert(*lg.begin());
+                lg.erase(lg.begin());
             }
         }
     }
-    inline T get() { return smq.top(); }
+    void erase(T x) {
+        if (sm.find(x) != sm.end()) {
+            sm.erase(sm.find(x));
+        } else {
+            lg.erase(lg.find(x));
+        }
+        if (sm.size() > lg.size() + 1) {
+            lg.insert(*sm.begin());
+            sm.erase(sm.begin());
+        }
+        if (sm.size() < lg.size()) {
+            sm.insert(*lg.begin());
+            lg.erase(lg.begin());
+        }
+    }
+    T get() {
+        if (sm.size() == lg.size()) {
+            return (*sm.begin() + *lg.begin()) / 2LL;
+        } else {
+            return *sm.begin();
+        }
+    }
 };
